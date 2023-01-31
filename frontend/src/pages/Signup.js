5e1429748b2ea5775e4193d355f42bdf3a41bb23
He Xiang Koh <kohhxx6@gmail.com>
2023-01-31 15:07:10 +0800

    Completed input component version 1

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Input from "../components/Input";
+import Button from "../components/Button";
+import { FaUser } from "react-icons/fa";
+
 
 const Signup = () => {
   return (
     <div>
-      <Input></Input>
-      <Input></Input>
+      <div className="text-center">
+        <h1 >
+          <FaUser className="inline mr-3"/> Register
+        </h1>
+        <p>Please create an account</p>
+      </div>
+      <form>
+        <Input type="text" label="Email" placeholder="Enter email" errorMessage="Please enter a valid email"></Input>
+        <Button primary rounded classNames="text-2xl">
+          Sign up
+        </Button>
+      </form>
     </div>
   );
 };
