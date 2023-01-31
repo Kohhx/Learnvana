95cd891d6af149929e62b8f13cc409701e951b8e
He Xiang Koh <kohhxx6@gmail.com>
2023-01-31 12:17:31 +0800

    Completed button component V1

---
+++ frontend/src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from "./Button"
+
+const Input = () => {
+  return (
+    <div><Button primary rounded classNames="text-2xl" >Hi there</Button></div>
+  )
+}
+
+export default Input
