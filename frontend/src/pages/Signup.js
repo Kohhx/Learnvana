29b98682ed143ef6fcb5d5d0d9589163d43c7d4d
He Xiang Koh <kohhxx6@gmail.com>
2023-01-31 17:01:08 +0800

    work on input component and signup form

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { FaUser } from "react-icons/fa";
-
+import Validator from "../utilities/Validator";
 
 const Signup = () => {
+  const formInitialState = {
+    email: {
+      value: "",
+      isValid: false,
+    },
+    password: {
+      value: "",
+      isValid: false,
+    }
+  };
+
+  const [inputData, setInputData] = useState(formInitialState);
+
+  // Handle all changes from all input and get back value and validity
+  const inputFormHandler = (input) => {};
+
   return (
     <div>
       <div className="text-center">
-        <h1 >
-          <FaUser className="inline mr-3"/> Register
+        <h1>
+          <FaUser className="inline mr-3" /> Register
         </h1>
         <p>Please create an account</p>
       </div>
       <form>
-        <Input type="text" label="Email" placeholder="Enter email" errorMessage="Please enter a valid email"></Input>
-        <Button primary rounded classNames="text-2xl">
+        <Input
+          id="email"
+          type="email"
+          label="Email"
+          placeholder="Enter email"
+          // errorMessage="Please enter a valid email"
+          validators={[
+            Validator.VALIDATOR_EMAIL(),
+            Validator.VALIDATOR_REQUIRE(),
+          ]}
+        ></Input>
+        <Input
+          id="password"
+          type="password"
+          label="Password"
+          placeholder="Please enter password"
+          // errorMessage="Please enter a valid email"
+          validators={[
+            Validator.VALIDATOR_REQUIRE(),
+            Validator.VALIDATOR_MINLENGTH(6),
+          ]}
+        ></Input>
+        <Button primary rounded>
           Sign up
         </Button>
       </form>
--- frontend/src/utilities/Validator.js
+++ frontend/src/utilities/Validator.js
@@ -1,49 +1,82 @@
-const VALIDATOR_TYPE_REQUIRE = 'REQUIRE';
-const VALIDATOR_TYPE_MINLENGTH = 'MINLENGTH';
-const VALIDATOR_TYPE_MAXLENGTH = 'MAXLENGTH';
-const VALIDATOR_TYPE_MIN = 'MIN';
-const VALIDATOR_TYPE_MAX = 'MAX';
-const VALIDATOR_TYPE_EMAIL = 'EMAIL';
-const VALIDATOR_TYPE_FILE = 'FILE';
+const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
+const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
+const VALIDATOR_TYPE_MAXLENGTH = "MAXLENGTH";
+const VALIDATOR_TYPE_MIN = "MIN";
+const VALIDATOR_TYPE_MAX = "MAX";
+const VALIDATOR_TYPE_EMAIL = "EMAIL";
+const VALIDATOR_TYPE_FILE = "FILE";
 
 // Validators used in the validate functions
 exports.VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 exports.VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
-exports.VALIDATOR_MINLENGTH = val => ({
+exports.VALIDATOR_MINLENGTH = (val) => ({
   type: VALIDATOR_TYPE_MINLENGTH,
-  val: val
+  val: val,
 });
-exports.VALIDATOR_MAXLENGTH = val => ({
+exports.VALIDATOR_MAXLENGTH = (val) => ({
   type: VALIDATOR_TYPE_MAXLENGTH,
-  val: val
+  val: val,
 });
-exports.VALIDATOR_MIN = val => ({ type: VALIDATOR_TYPE_MIN, val: val });
-exports.VALIDATOR_MAX = val => ({ type: VALIDATOR_TYPE_MAX, val: val });
+exports.VALIDATOR_MIN = (val) => ({ type: VALIDATOR_TYPE_MIN, val: val });
+exports.VALIDATOR_MAX = (val) => ({ type: VALIDATOR_TYPE_MAX, val: val });
 exports.VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
-
 
 // Validate Function used to validate, validators is an array
 exports.validate = (value, validators) => {
+  let errorMessages = [];
   let isValid = true;
   for (const validator of validators) {
     if (validator.type === VALIDATOR_TYPE_REQUIRE) {
-      isValid = isValid && value.trim().length > 0;
+      const typeIsValid = value.trim().length > 0;
+      if (!typeIsValid) {
+        errorMessages.push("An input is required");
+      }
+      isValid = isValid && typeIsValid;
     }
     if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
-      isValid = isValid && value.trim().length >= validator.val;
+      const typeIsValid = value.trim().length >= validator.val;
+      if (!typeIsValid) {
+        errorMessages.push(
+          `Please enter input with minimum length of ${validator.val}`
+        );
+      }
+      isValid = isValid && typeIsValid;
     }
     if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
-      isValid = isValid && value.trim().length <= validator.val;
+      const typeIsValid = value.trim().length <= validator.val;
+      if (!typeIsValid) {
+        errorMessages.push(
+          `Please enter input with maximum length of ${validator.val}`
+        );
+      }
+      isValid = isValid && typeIsValid;
     }
     if (validator.type === VALIDATOR_TYPE_MIN) {
-      isValid = isValid && +value >= validator.val;
+      const typeIsValid = +value >= validator.val;
+      if (!typeIsValid) {
+        errorMessages.push(
+          `Please enter input value more than or equal to ${validator.val}`
+        );
+      }
+      isValid = isValid && typeIsValid;
     }
     if (validator.type === VALIDATOR_TYPE_MAX) {
-      isValid = isValid && +value <= validator.val;
+      const typeIsValid = +value <= validator.val;
+      if (!typeIsValid) {
+        errorMessages.push(
+          `Please enter input value less than or equal to ${validator.val}`
+        );
+        isValid = isValid && typeIsValid;
+      }
     }
     if (validator.type === VALIDATOR_TYPE_EMAIL) {
-      isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+      const typeIsValid = /^\S+@\S+\.\S+$/.test(value);
+      if (!typeIsValid) {
+        errorMessages.push(`Please enter valid email`);
+      }
+      isValid = isValid && typeIsValid;
     }
   }
-  return isValid;
+
+  return [isValid, errorMessages];
 };
