0e8609279e67457c816d2a3a4d99d6b3f241aaec
He Xiang Koh <kohhxx6@gmail.com>
2023-02-09 19:48:37 +0800

    added css transition package and apply to side bar

--- frontend/src/pages/InstructorClass.js
+++ frontend/src/pages/InstructorClass.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { reset, getInstructorClass } from "../features/class/classSlice";
 import Hamburger from "../components/Shared/Hamburger";
+import { CSSTransition } from "react-transition-group";
 import "../components/Shared/Hamburger.css";
 
 const InstructorClass = () => {
@@ -28,7 +29,14 @@ const InstructorClass = () => {
   return (
     <div>
       <div className="flex">
-        {isOpen && <div className="h-screen w-[13%] bg-orange-400"></div>}
+        <CSSTransition
+          in={isOpen}
+          timeout={200}
+          classNames="fade"
+          unmountOnExit
+        >
+          <div className="h-screen w-[13%] bg-orange-400"></div>
+        </CSSTransition>
         <div>
           <Hamburger
             classNames="m-2"
