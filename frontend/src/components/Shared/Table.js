a9992e064c8955e94e74b5fcde394194e510c58b
He Xiang Koh <kohhxx6@gmail.com>
2023-02-14 11:42:19 +0800

    Added indexing function for table component

--- frontend/src/components/Shared/Table.js
+++ frontend/src/components/Shared/Table.js
@@ -7,17 +7,27 @@ const Table = ({ data, config }) => {
     if (dataCol.header) {
       return <Fragment key={i}>{dataCol.header()}</Fragment>;
     }
-    return <th key={i} >{dataCol.label}</th>;
+    return <th key={i}>{dataCol.label}</th>;
   });
 
   // Create body content
   const tableBodyContent = data.map((dataRow, i) => {
     const rowContent = config.map((dataCol, j) => {
-      return (
-        <td key={j} className="px-10">
-          {dataCol.render(dataRow)}
-        </td>
-      );
+      let dataColContent;
+      if (dataCol.index) {
+        dataColContent = (
+          <td key={j} className="px-10">
+            {dataCol.render(i + 1)}
+          </td>
+        );
+      } else {
+        dataColContent = (
+          <td key={j} className="px-10">
+            {dataCol.render(dataRow)}
+          </td>
+        );
+      }
+      return dataColContent;
     });
     return <tr key={i}>{rowContent}</tr>;
   });
