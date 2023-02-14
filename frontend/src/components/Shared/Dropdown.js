ccfba89330193d32a9f52c7ca89a90c092c672c3
He Xiang Koh <kohhxx6@gmail.com>
2023-02-14 19:52:03 +0800

    Fix mroe dropdown bugs

--- frontend/src/components/Shared/Dropdown.js
+++ frontend/src/components/Shared/Dropdown.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 const DropDown = ({ children, configs }) => {
   const dropdownDivEl = useRef();
-  // const dropdownUlEl = useRef();
   const [isOpen, setIsOpen] = useState();
+  console.log(isOpen);
 
   useEffect(() => {
     const handler = (event) => {
-      if (!dropdownDivEl.current.contains(event.target)) {
+      if (
+        dropdownDivEl.current &&
+        !dropdownDivEl.current.contains(event.target)
+      ) {
         setIsOpen(false);
       }
     };
-
     document.addEventListener("click", handler, true);
 
     return () => {
-      document.removeEventListener("click", handler);
+      document.removeEventListener("click", handler, true);
     };
   }, []);
 
+  console.log("Render");
+
   const dropdownContent = (
-    <div
-      className="absolute right-0 top-10 w-[250px] rounded-md bg-orange-300 p-4"
-      ref={dropdownDivEl}
-    >
+    <div className="absolute right-0 top-10 w-[250px] rounded-md bg-orange-300 p-4">
       <ul className="flex flex-col">
         {configs.map((config) => {
           let liContent;
@@ -34,7 +36,7 @@ const DropDown = ({ children, configs }) => {
             liContent = <div>{config.button}</div>;
           }
           return (
-            <div  onClick={() => setIsOpen(false)} className="mb-2 w-fit">
+            <div onClick={() => setIsOpen(false)} className="mb-2 w-fit">
               {liContent}
             </div>
           );
@@ -44,9 +46,17 @@ const DropDown = ({ children, configs }) => {
   );
 
   return (
-    <div className="relative">
-      <div onClick={() => setIsOpen(!isOpen)}>{children}</div>
-      {isOpen && dropdownContent}
+    <div className="relative" ref={dropdownDivEl}>
+      <div onClick={() => setIsOpen((prev) => !prev)}>{children}</div>
+
+      <CSSTransition
+        in={isOpen}
+        timeout={400}
+        classNames="grow" // Classes for css transition in index.css
+        unmountOnExit
+      >
+        {dropdownContent}
+      </CSSTransition>
     </div>
   );
 };
