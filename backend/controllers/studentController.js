a2f08d8b7a3a2505ad192e5129014748a16b7d01
leon boon <l.yung5982@gmail.com>
2023-02-19 16:12:50 +0800

    student can view lessons and a lesson

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -5,9 +5,12 @@ const Class = require("../models/class");
 
 
 
-// @desc create a new student
-// @route /api/student/create
-// @access user
+/**
+ * =============================================================================
+ * Student
+ * =============================================================================
+ */
+
 exports.createStudent = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
   console.log(user);
@@ -77,6 +80,13 @@ exports.createStudent = asyncHandler(async (req, res) => {
   }]);
 });
 
+
+/**
+ * =============================================================================
+ * Student Classes
+ * =============================================================================
+ */
+
 exports.getStudentClasses = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
   const student = await Student.findById(user.studentprofiles).populate(
@@ -115,4 +125,134 @@ exports.getAllStudentsFromClass = asyncHandler(async (req, res) => {
 
   // return pending students
   res.status(201).json(classFound.students);
+});
+
+
+/**
+ * =============================================================================
+ * Student class lessons
+ * =============================================================================
+ */
+
+exports.getClassLessons = asyncHandler(async (req, res) => {
+  const classId = req.params.classId;
+
+  let classFound;
+  try {
+    classFound = await Class.findById(classId);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error);
+  }
+
+  const user = await User.findById(req.user.id);
+  const student = await Student.findById(user.studentprofiles);
+  // const instructor = await Instructor.findById(user.instructorprofile).populate(
+  //   { "classes" }
+  // );
+  //
+
+  if (!user) {
+    res.status(400);
+    throw new Error("No user found");
+  }
+
+  if (user.role !== "student") {
+    res.status(400);
+    throw new Error("User is not a student");
+  }
+
+  if (!student) {
+    res.status(400);
+    throw new Error(
+      "No student profile created. Create student profile first"
+    );
+  }
+
+  if (!classFound.students.toString() === user._id.toString()) {
+    res.status(400);
+    throw new Error("Class does not belong to this student");
+  }
+
+  const classLessons = classFound.lessons;
+
+  console.log(classLessons);
+
+  res.status(201).json(classLessons);
+});
+
+
+// get one lesson from a class
+
+exports.getClassLesson = asyncHandler(async (req, res) => {
+  const classId = req.params.classId;
+  const lessonId = req.params.lessonId;
+  // const {classId, lessonId} = req.params;
+  // javascript destructruting
+
+
+  let classFound;
+  try {
+    classFound = await Class.findById(classId);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error);
+  }
+
+  // String.prototype.toObjectId = function() {
+  //   var ObjectId = (require('mongoose').Types.ObjectId);
+  //   return new ObjectId(this.toString());
+  // };
+  // console.log(lessonId.toObjectId())
+  // console.log(classFound.lessons[3]._id)
+  // console.log(classFound.lessons[3].id === lessonId)
+  function findLesson(lessonId) {
+    for (let i=0; i < classFound.lessons.length; i++) {
+      if (classFound.lessons[i].id === lessonId) {
+        // console.log(i);
+        // x = i;
+        return i;
+      }
+    }
+  };
+
+  let lessonFound;
+  try {
+    lessonFound = await classFound.lessons[findLesson(lessonId)]
+  } catch (error) {
+    res.status(400);
+    throw new Error(error);
+  }
+
+  const user = await User.findById(req.user.id);
+  const student = await Student.findById(user.studentprofiles).populate(
+    { path: 'classes', select: 'lessons' }
+  );
+
+  if (!user) {
+    res.status(400);
+    throw new Error("No user found");
+  }
+
+  if (user.role !== "student") {
+    res.status(400);
+    throw new Error("User is not a student");
+  }
+
+  if (!student) {
+    res.status(400);
+    throw new Error(
+      "No student profile created. Create student profile first"
+    );
+  }
+
+  if (!classFound.students.toString() === user._id.toString()) {
+    res.status(400);
+    throw new Error("Class does not belong to this instructor");
+  }
+
+
+  console.log(lessonFound);
+
+  res.status(201).json(lessonFound);
 });
