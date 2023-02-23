b1d535cff80b5272bb073a1a421937275503e398
He Xiang Koh <kohhxx6@gmail.com>
2023-02-23 15:44:26 +0800

    Guardian can connact to students

--- frontend/src/components/Classitem.js
+++ frontend/src/components/Classitem.js
@@ -5,7 +5,7 @@ const Classitem = ({ classData, role }) => {
   const { title, _id } = classData;
   return (
     <div>
-      <Link to={`/${role}s/classes/${_id}`}>{title}</Link>
+      <Link to={`/students/classes/${_id}`}>{title}</Link>
     </div>
   );
 };
