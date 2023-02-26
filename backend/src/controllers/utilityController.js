bccfc8e34eb71da33140fb3e6d41981548979067
He Xiang Koh <kohhxx6@gmail.com>
2023-02-26 12:19:33 +0800

    Add more function to editorjs. Add all logic for attach files, upload video etc

--- backend/src/controllers/utilityController.js
+++ backend/src/controllers/utilityController.js
@@ -5,7 +5,8 @@ const cloudinaryHelper = require("../utility/cloudinaryHelper");
 // @route /api/utilities/uploadphoto
 // @access Private
 exports.editorJSUploadPhoto = asyncHandler(async (req, res, next) => {
-
+  const { file } = req;
+  const { mimetype } = file;
   const { secure_url, public_id } = await cloudinaryHelper.uploadPhoto(
     req.file
   );
@@ -15,14 +16,63 @@ exports.editorJSUploadPhoto = asyncHandler(async (req, res, next) => {
     file: {
       url: secure_url,
       public_id,
+      mimetype,
       // any other image data you want to store, such as width, height, color, extension, etc
     },
   });
 });
 
-exports.editorJSDeletePhoto = asyncHandler(async (req, res, next) => {
-  const { public_id }= req.body;
-  // console.log(public_id)
-  const destroRes = await cloudinaryHelper.deletePhoto(public_id);
-  res.status(200).json({ message: "Photo delete from database" });
+exports.editorJSUploadVideo = asyncHandler(async (req, res, next) => {
+  const { file } = req;
+  const { mimetype } = file;
+  const { secure_url, public_id } = await cloudinaryHelper.uploadVideo(
+    req.file
+  );
+
+  res.status(200).json({
+    success: 1,
+    file: {
+      url: secure_url,
+      public_id,
+      mimetype,
+    },
+  });
+});
+
+exports.editorJSDeleteFile = asyncHandler(async (req, res, next) => {
+  const { public_id, mimetype } = req.body;
+  // console.log("PublicID", public_id);
+
+  const mimetypeArr = mimetype.split("/");
+
+  let deleteType = "image";
+  if (mimetypeArr[0] === "video") {
+    deleteType = "video";
+  }
+
+  if (mimetypeArr[0] === "application") {
+    if (mimetypeArr[1] !== "pdf") {
+      deleteType = "raw";
+    }
+  }
+
+  const destroRes = await cloudinaryHelper.deleteFile(public_id, deleteType);
+  res.status(200).json({ message: "File deleted from database" });
+});
+
+exports.editorJSUploadFile = asyncHandler(async (req, res, next) => {
+  const { file } = req;
+  const { mimetype } = file;
+  const { secure_url, public_id } = await cloudinaryHelper.uploadAllFiles(
+    req.file
+  );
+
+  res.status(200).json({
+    success: 1,
+    file: {
+      url: secure_url,
+      public_id,
+      mimetype,
+    },
+  });
 });
